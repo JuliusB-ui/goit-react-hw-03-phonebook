c5b38288058762ce3b18f1e7a3e1366c8ac56825
JuliusB-ui <baraquiel.julius@gmail.com>
2024-06-22 20:58:44 +0700

    added  componentDidMount and componentDidUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,31 @@ import { List } from './List/List';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
+
+  componentDidMount() {
+    // Check if contacts are saved in localStorage
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      // If yes, update the state with these contacts
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // Check if current contacts are different from the previous state
+    if (prevState.contacts !== this.state.contacts) {
+      // If they are different, save the current contacts to localStorage
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = newContact => {
     this.setState(prevState => ({
